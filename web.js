542dcc9f792e3bcb178698d112f4c58ee3e3ff24
Andrew Frankel <andrew@leknarf.net>
2013-07-05 17:29:32 -0400

    Read hello world from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+var filename = 'index.html';
+
+var hello_buffer = fs.readFileSync(filename);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(hello_buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
